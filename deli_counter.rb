a47b2bd06c96b278e93639ed4fda772297e7239d
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-13 13:19:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line (array)
     puts "The line is currently empty."
   else
   output = String.new
-  output << "The line is currently"
+  output << "The line is currently: "
   array.each_with_index do |x,i|
     output << "#{i+1}. #{x} "
   end
