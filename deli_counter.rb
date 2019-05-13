2675a1bc80181c6276be550f1c424c52ccc31e16
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-13 13:16:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line (array)
   if array.size == 0
     puts "The line is currently empty."
   else
-  print "The line is currently"
+  puts "The line is currently"
   array.each_with_index do |x,i|
     print "#{i+1}. #{x} "
   end
